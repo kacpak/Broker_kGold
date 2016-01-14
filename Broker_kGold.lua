ef3e4fc9028569a1c973d56a5fea574fe423c629
Mateusz Kasprzak <kacpak@gmail.com>
2016-01-14 19:56:04 +0100

    Fixed License Boilerplate

--- Broker_kGold.lua
+++ Broker_kGold.lua
@@ -2,7 +2,7 @@
 Name: Broker kGold
 Description: Tracks your gold
 
-Copyright 2008 Quaiche of Dragonblight
+Copyright 2016 Mateusz Kasprzak
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
